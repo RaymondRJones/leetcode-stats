7b58197dc94dd667d69a7fc98780d7689cb4166d
RaymondJones <Rayjones2170@gmail.com>
2024-04-14 14:55:08 -0400

    Adds a function to get the predicted rating from the lccnbao site

--- query_scripts/query_predicted_elo.py
+++ query_scripts/query_predicted_elo.py
@@ -8,15 +8,14 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0'
 }
 
-def get_delta_rating_of_user(username, contest_name):
+def get_new_rating_of_user(username, contest_name):
     url = f"https://lccn.lbao.site/api/v1/contest-records/user?contest_name={contest_name}&username={username}&archived=false"
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         if data and isinstance(data, list) and len(data) > 0:
-            return data[0].get('delta_rating')
-    print('Failed to retrieve data for {}: {}'.format(username, response.status_code))
-    print("They probably didn't take the contest...")
+            return data[0].get('new_rating')
+    print("No Data... They probably didn't take the contest...")
     return None
 
 def load_existing_elos(filename):
@@ -25,9 +24,9 @@ def load_existing_elos(filename):
 
 def update_elos_with_new_ratings(existing_elos, user_ratings):
     elo_dict = {user['name']: user for user in existing_elos}
-    for username, delta in user_ratings:
+    for username, new_rating in user_ratings:
         if username in elo_dict:
-            elo_dict[username]['elo'] += delta
+            elo_dict[username]['elo'] = new_rating
     return list(elo_dict.values())
 
 def write_elos_to_json(filename, updated_elos):
@@ -40,11 +39,11 @@ def main():
     user_ratings = []
     for username in usernames:
         print("Getting delta rating of...", username)
-        delta_rating = get_delta_rating_of_user(username, "weekly-contest-388")
-        if delta_rating is not None:
-            delta = int(delta_rating)
-            user_ratings.append((username, delta))
-            print("Success! Adding delta rating", delta, "to", username)
+        new_rating = get_new_rating_of_user(username, "weekly-contest-393")
+        if new_rating is not None:
+            new_rating = int(new_rating)
+            user_ratings.append((username, new_rating))
+            print("Success! Adding delta rating", new_rating, "to", username)
     updated_elos = update_elos_with_new_ratings(existing_elos, user_ratings)
     write_elos_to_json('../leetcode-elo/public/users_by_elo.json', updated_elos)
 
